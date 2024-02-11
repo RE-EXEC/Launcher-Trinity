3db50ce5fe11f6d92d588d28fe3110f61c56c84f
Kristian plamenov <kristianzvezdakris@gmail.com>
2024-02-11 20:20:26 +0100

    coms

--- target/scala-2.13/classes/controllers/Log.scala
+++
@@ -1,8 +0,0 @@
-
-package net.k3nder.lt.controllers
-
-import net.kender.logger.log5k.conf.CustomLogger
-
-
-class Log(c: Class[?]) extends CustomLogger(c,"LT"){
-}
--- target/scala-2.13/classes/json/parser/asset.scala
+++
@@ -1,9 +0,0 @@
-package net.k3nder.lt
-package json.parser
-
-import com.fasterxml.jackson.annotation.JsonProperty
-
-class asset {
-  @JsonProperty("file") var file: String = _
-  @JsonProperty("url") var url: String = _
-}
